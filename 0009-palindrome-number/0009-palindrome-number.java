fe225410a67ca36e4c596a1d115d1c90b1719cc3
Ajinkya  Kokate <148894276+Ajinkya47@users.noreply.github.com>
2026-07-14 22:44:51 +0530

    Time: 5 ms (80.61%), Space: 45.7 MB (87.6%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -12,5 +12,29 @@ class Solution {
         }
         
         return original == revNum;
-    }
+
+        // if(x<0)return false;
+
+
+
+    //     int num=palin(x);
+
+    //     if(num==x){
+    //         return true;
+    //     }
+
+    //     return false;
+       
+    // }
+    // int reverse=0;
+    // public int palin(int x){
+    //     if(x<1) return 0;
+
+        
+    //     reverse=reverse*10+(x%10);
+
+    //     palin(x/10);
+    //     return reverse;
+    // }
+}
 }
